ed4c0d7e6fa7a8b5359b892f308f2b85ea0ccf4c
Gabriel Mesquida Masana <gabriel@MB12gabriel.local>
2020-05-18 17:26:10 +1000

    List.of

--- Test.java
+++ Test.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.List;
 
 public class Test {
 
@@ -8,13 +8,13 @@ public class Test {
          */
         Automaton nfa = new Automaton("NFA1");
         // Symbols
-        nfa.addSymbol(Arrays.asList("a", "b"));
+        nfa.addSymbol(List.of("a", "b"));
         // States
-        nfa.addState(Arrays.asList("q0", "q1"));
+        nfa.addState(List.of("q0", "q1"));
         // Transitions
         nfa.getState("q0")
-                .setTransition(Arrays.asList("a", "q0", "a", "q1", "b", "q1"));
-        nfa.getState("q1").setTransition(Arrays.asList("b", "q0"));
+                .setTransition(List.of("a", "q0", "a", "q1", "b", "q1"));
+        nfa.getState("q1").setTransition(List.of("b", "q0"));
         // Accept states
         nfa.setAccept("q0");
         // Start
@@ -36,16 +36,15 @@ public class Test {
          */
         Automaton nfa = new Automaton("NFA2");
         // Symbols
-        nfa.addSymbol(Arrays.asList("a", "b"));
+        nfa.addSymbol(List.of("a", "b"));
         // States
-        nfa.addState(Arrays.asList("q0", "q1", "q2"));
+        nfa.addState(List.of("q0", "q1", "q2"));
         // Transitions
         nfa.getState("q0")
-                .setTransition(
-                        Arrays.asList("a", "q2", Automaton.EPSILON, "q1"));
-        nfa.getState("q1").setTransition(Arrays.asList("a", "q0"));
+                .setTransition(List.of("a", "q2", Automaton.EPSILON, "q1"));
+        nfa.getState("q1").setTransition(List.of("a", "q0"));
         nfa.getState("q2")
-                .setTransition(Arrays.asList("a", "q1", "b", "q1", "b", "q2"));
+                .setTransition(List.of("a", "q1", "b", "q1", "b", "q2"));
         // Accept states
         nfa.setAccept("q1");
         // Start
@@ -82,15 +81,15 @@ public class Test {
          */
         Automaton nfa = new Automaton("NFA3");
         // Symbols
-        nfa.addSymbol(Arrays.asList("0", "1"));
+        nfa.addSymbol(List.of("0", "1"));
         // States
-        nfa.addState(Arrays.asList("q0", "q1", "q2"));
+        nfa.addState(List.of("q0", "q1", "q2"));
         // Transitions
-        nfa.getState("q0").setTransition(Arrays.asList("0", "q1", "1", "q1"));
+        nfa.getState("q0").setTransition(List.of("0", "q1", "1", "q1"));
         nfa.getState("q1")
-                .setTransition(Arrays.asList("0", "q0", "0", "q2", "1", "q1",
+                .setTransition(List.of("0", "q0", "0", "q2", "1", "q1",
                         Automaton.EPSILON, "q2"));
-        nfa.getState("q2").setTransition(Arrays.asList("1", "q1"));
+        nfa.getState("q2").setTransition(List.of("1", "q1"));
         // Accept states
         nfa.setAccept("q1");
         // Start
@@ -122,20 +121,20 @@ public class Test {
          */
         Automaton nfa = new Automaton("NFA4");
         // Symbols
-        nfa.addSymbol(Arrays.asList("1"));
+        nfa.addSymbol(List.of("1"));
         // States
-        nfa.addState(Arrays.asList("s", "q1", "q2", "r1", "r2", "r3"));
+        nfa.addState(List.of("s", "q1", "q2", "r1", "r2", "r3"));
         // Transitions
         nfa.getState("s")
-                .setTransition(Arrays.asList(Automaton.EPSILON, "q1",
+                .setTransition(List.of(Automaton.EPSILON, "q1",
                         Automaton.EPSILON, "r1"));
-        nfa.getState("q1").setTransition(Arrays.asList("1", "q2"));
-        nfa.getState("q2").setTransition(Arrays.asList("1", "q1"));
-        nfa.getState("r1").setTransition(Arrays.asList("1", "r2"));
-        nfa.getState("r2").setTransition(Arrays.asList("1", "r3"));
-        nfa.getState("r3").setTransition(Arrays.asList("1", "r1"));
+        nfa.getState("q1").setTransition(List.of("1", "q2"));
+        nfa.getState("q2").setTransition(List.of("1", "q1"));
+        nfa.getState("r1").setTransition(List.of("1", "r2"));
+        nfa.getState("r2").setTransition(List.of("1", "r3"));
+        nfa.getState("r3").setTransition(List.of("1", "r1"));
         // Accept states
-        nfa.setAccept(Arrays.asList("q1", "r1"));
+        nfa.setAccept(List.of("q1", "r1"));
         // Start
         nfa.setStart("s");
         nfa.render(); // Show
